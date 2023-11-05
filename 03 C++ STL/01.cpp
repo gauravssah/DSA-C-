cb49fbd95177188b9c8a64486d5e27dd0a7411da
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-05 15:48:30 +0530

    new vid started

---
+++ 03 C++ STL/01.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printName()
+{
+    cout << "Gaurav sah" << endl;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    printName();
+    int s = sum(4, 5);
+    cout << s << endl;
+    return 0;
+}
