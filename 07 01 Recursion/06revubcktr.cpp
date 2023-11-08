db701c3991b88db8506b7a4494399b6089a2f058
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-08 21:16:55 +0530

    reversing using backtracking

---
+++ 07 01 Recursion/06revubcktr.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void reverprintingusingbacktracking(int i, int n)
+{
+    if (i > n)
+    {
+        return;
+    }
+
+    reverprintingusingbacktracking(i + 1, n);
+    cout << i << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter Your Number :" << endl;
+    cin >> n;
+
+    cout << "start printing.." << endl;
+    reverprintingusingbacktracking(1, n);
+    cout << "End printing.." << endl;
+    return 0;
+}
