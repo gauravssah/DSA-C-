195e332b605e979c673c16fce3587f7c131885d0
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-08 20:50:45 +0530

    reverse number printing

---
+++ 07 01 Recursion/04reverseN.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void printingInReverse(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    cout << n << endl;
+    printingInReverse(n - 1);
+}
+
+int main()
+{
+    cout << "Enter The Number :" << endl;
+    int n;
+    cin >> n;
+    cout << "Starting Printing..." << endl;
+    printingInReverse(n);
+
+    cout << "Printing End.." << endl;
+    return 0;
+}
