39424e6a4eb04536513e5d854956274b09c10e16
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-30 17:36:01 +0530

    re-02 done

---
+++ 08 02 Recursion/3_Basic-recusion-problems.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// -------- Q. [01] -------->
+
+int counts = 0;
+string firstName, lastName;
+void printMyNames(string name, string lname)
+{
+    if (counts == 5)
+    {
+        return;
+    }
+    cout << counts << ". " << name << " " << lname << endl;
+    counts++;
+    printMyNames(firstName, lastName);
+}
+
+// -------- Q. [02] -------->
+
+int num;
+int numCounter = 1;
+void printNumberFrom1ToN(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    cout << numCounter << endl;
+    numCounter++;
+    n--;
+    printNumberFrom1ToN(n);
+}
+
+// -------- Q. [03] -------->
+
+void printNumberFromNto1(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    cout << n << endl;
+    n--;
+    printNumberFromNto1(n);
+}
+
+// -------- Q. [04] -------->
+
+void printing1ToNbyBacktracking(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    // n--;
+    printing1ToNbyBacktracking(n - 1);
+    cout << n << endl;
+}
+
+// -------- Q. [05] -------->
+
+void printingNto1ByBacktracking(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+    cout << n << endl;
+    printingNto1ByBacktracking(n - 1);
+}
+
+int main()
+{
+    // Q. 01. Print Names 5 times. ----->
+
+    // cout << "Hey, please Enter Your Full Name : " << endl;
+    // cin >> firstName >> lastName;
+    // printMyNames(firstName, lastName);
+
+    // ####-----####-----####-----####-----####
+
+    // Q. 02. Print linearly from 1 to N. ----->
+
+    // cout << "Enter The Number. " << endl;
+    // cin >> num;
+    // cout << "Start Printing..." << endl;
+    // printNumberFrom1ToN(num);
+
+    // ####-----####-----####-----####-----####
+
+    // Q. 03. Print linearly from N to 1. ----->
+
+    // cout << "Enter The Number. " << endl;
+    // cin >> num;
+    // cout << "Start Printing..." << endl;
+    // printNumberFromNto1(num);
+
+    // ####-----####-----####-----####-----####
+
+    // Q. 04. Print linearly from 1 to N (By backtracking). ----->
+
+    // cout << "Enter The Number. " << endl;
+    // cin >> num;
+    // cout << "Start Printing (By backtracking)..." << endl;
+    // printing1ToNbyBacktracking(num);
+
+    // ####-----####-----####-----####-----####
+
+    // Q. 05. Print linearly from N to 1 (By backtracking). ----->
+
+    cout << "Enter The Number. " << endl;
+    cin >> num;
+    cout << "Start Printing (By backtracking)..." << endl;
+    printingNto1ByBacktracking(num);
+
+    return 0;
+}
